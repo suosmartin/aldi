4508b6d480617a545c1bf8fe9a3ae57137d45665
suosmartin <suosmartin@gmail.com>
2025-06-27 13:08:53 +0200

    missing observable handling

--- src/app/features/products/components/cart-sidebar/cart-sidebar.component.ts
+++ src/app/features/products/components/cart-sidebar/cart-sidebar.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectCart, selectProducts } from '../../store/products.selectors';
 import { CartItemCardComponent } from '../cart-item-card/cart-item-card.component';
 import { AsyncPipe, CommonModule, CurrencyPipe } from '@angular/common';
 import { clearCart, updateCartItem } from '../../store/products.actions';
 import { RouterModule } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-cart-sidebar',
@@ -18,25 +19,32 @@ import { RouterModule } from '@angular/router';
   templateUrl: './cart-sidebar.component.html',
   styleUrl: './cart-sidebar.component.scss',
 })
-export class CartSidebarComponent {
+export class CartSidebarComponent implements OnDestroy {
   private store = inject(Store);
   cart$ = this.store.select(selectCart);
   products$ = this.store.select(selectProducts);
   totalPrice = 0;
   productsMap: { [id: string]: any } = {};
+  private destroy$ = new Subject<void>();
 
   constructor() {
-    this.products$.subscribe((products) => {
+    this.products$.pipe(takeUntil(this.destroy$)).subscribe((products) => {
       this.productsMap = {};
       products.forEach((p) => (this.productsMap[p.id] = p));
     });
-    this.cart$.subscribe((cart) => {
+    this.cart$.pipe(takeUntil(this.destroy$)).subscribe((cart) => {
       this.totalPrice = cart.reduce((total, item) => {
         const product = this.productsMap[item.productId];
         return total + (product ? product.price * item.quantity : 0);
       }, 0);
     });
   }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   onQuantityChange(productId: string, quantity: number) {
     this.store.dispatch(updateCartItem({ item: { productId, quantity } }));
   }
--- src/app/features/products/pages/cart/cart.component.ts
+++ src/app/features/products/pages/cart/cart.component.ts
@@ -1,9 +1,10 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { CartItemCardComponent } from '../../components/cart-item-card/cart-item-card.component';
 import { updateCartItem, clearCart } from '../../store/products.actions';
 import { selectCart, selectProducts } from '../../store/products.selectors';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
@@ -12,24 +13,30 @@ import { selectCart, selectProducts } from '../../store/products.selectors';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
-export class CartComponent {
+export class CartComponent implements OnDestroy {
   private store = inject(Store);
   cart$ = this.store.select(selectCart);
   products$ = this.store.select(selectProducts);
   totalPrice = 0;
   productsMap: { [id: string]: any } = {};
+  private destroy$ = new Subject<void>();
 
   constructor() {
-    this.products$.subscribe((products) => {
+    this.products$.pipe(takeUntil(this.destroy$)).subscribe((products) => {
       this.productsMap = {};
       products.forEach((p) => (this.productsMap[p.id] = p));
     });
-    this.cart$.subscribe((cart) => {
+    this.cart$.pipe(takeUntil(this.destroy$)).subscribe((cart) => {
       this.totalPrice = cart.reduce((total, item) => {
         const product = this.productsMap[item.productId];
         return total + (product ? product.price * item.quantity : 0);
       }, 0);
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   onQuantityChange(productId: string, quantity: number) {
